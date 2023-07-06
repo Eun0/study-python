d59553154d17bc31a0b351c7a992bd1f41424230
Eun Yeong Jeon <33515697+Eun0@users.noreply.github.com>
2023-07-07 08:19:45 +0900

    Update client.py

--- client.py
+++ client.py
@@ -1,4 +1,4 @@
-class Client(ConnectionBase):
+class AsyncClient(AsyncConnectionBase):
     def __init__(self, *args):
         super().__init__(*args)
         self._clear_state()
@@ -7,27 +7,27 @@ class Client(ConnectionBase):
         self.secret = None
         self.last_distance = None
 
-    @contextlib.contextmanager
-    def session(self, lower, upper, secret):
+    @contextlib.asynccontextmanager                         # 변경됨
+    async def session(self, lower, upper, secret):          # 변경됨
         print(f'\n{lower}와 {upper} 사이의 숫자를 맞춰보세요!'
               f' 쉿! 그 숫자는 {secret} 입니다.')
         self.secret = secret
-        self.send(f'PARAMS {lower} {upper}')
+        await self.send(f'PARAMS {lower} {upper}')          # 변경됨
         try:
             yield
         finally:
             self._clear_state()
-            self.send('PARAMS 0 -1')
+            await self.send('PARAMS 0 -1')                   # 변경됨
 
-    def request_numbers(self, count):
+    async def request_numbers(self, count):            # 변경됨
         for _ in range(count):
-            self.send('NUMBER')
-            data = self.receive()
+            await self.send('NUMBER')                  # 변경됨
+            data = await self.receive()                # 변경됨
             yield int(data)
             if self.last_distance == 0:
                 return
 
-    def report_outcome(self, number):
+    async def report_outcome(self, number):                    # 변경됨
         new_distance = math.fabs(number - self.secret)
         decision = UNSURE
 
@@ -42,5 +42,8 @@ class Client(ConnectionBase):
 
         self.last_distance = new_distance
 
-        self.send(f'REPORT {decision}')
+        await self.send(f'REPORT {decision}')                  # 변경됨
+
+        # 잠시 대기해서 출력 순서 조정
+        await asyncio.sleep(0.01)
         return decision
