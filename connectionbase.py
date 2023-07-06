7840d4b1ffec464a8779aca120cbd63af9733748
Eun Yeong Jeon <33515697+Eun0@users.noreply.github.com>
2023-07-07 07:59:42 +0900

    Create connectionbase.py

---
+++ connectionbase.py
@@ -0,0 +1,18 @@
+class EOFError(Exception):
+    pass
+
+class ConnectionBase:
+    def __init__(self, connection):
+        self.connection = connection
+        self.file = connection.makefile('rb')
+
+    def send(self, command):
+        line = command + '\n'
+        data = line.encode()
+        self.connection.send(data)
+
+    def receive(self):
+        line = self.file.readline()
+        if not line:
+            raise EOFError('연결 닫힘')
+        return line[:-1].decode()
