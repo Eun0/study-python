1194e8df7289f4900eef24274f412645c25ffcd6
Eun Yeong Jeon <33515697+Eun0@users.noreply.github.com>
2023-07-07 08:00:04 +0900

    Update connectionbase.py

--- connectionbase.py
+++ connectionbase.py
@@ -1,18 +1,19 @@
 class EOFError(Exception):
     pass
 
-class ConnectionBase:
-    def __init__(self, connection):
-        self.connection = connection
-        self.file = connection.makefile('rb')
+class AsyncConnectionBase:
+    def __init__(self, reader, writer):  # 변경됨
+        self.reader = reader  # 변경됨
+        self.writer = writer  # 변경됨
 
-    def send(self, command):
+    async def send(self, command):
         line = command + '\n'
         data = line.encode()
-        self.connection.send(data)
+        self.writer.write(data)  # 변경됨
+        await self.writer.drain()  # 변경됨
 
-    def receive(self):
-        line = self.file.readline()
+    async def receive(self):
+        line = await self.reader.readline()  # 변경됨
         if not line:
             raise EOFError('연결 닫힘')
         return line[:-1].decode()
