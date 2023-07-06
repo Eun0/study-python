90b74d988af0148d82d9c5de7a61edff84fc1ec5
Eun Yeong Jeon <33515697+Eun0@users.noreply.github.com>
2023-07-07 08:22:40 +0900

    Create run.py

---
+++ run.py
@@ -0,0 +1,51 @@
+import socket
+from threading import Thread
+
+
+def handle_connection(connection):
+    with connection:
+        session = Session(connection)
+        try:
+            session.loop()
+        except EOFError:
+            pass
+
+
+def run_server(address):
+    with socket.socket() as listener:
+        listener.bind(address)
+        listener.listen()
+        while True:
+            connection, _ = listener.accept()
+            thread = Thread(target=handle_connection,
+                            args=(connection,),
+                            daemon=True)
+            thread.start()
+
+def run_client(address):
+    with socket.create_connection(address) as connection:
+        client = Client(connection)
+
+        with client.session(1, 5, 3):
+            results = [(x, client.report_outcome(x))
+                       for x in client.request_numbers(5)]
+
+        with client.session(10, 15, 12):
+            for number in client.request_numbers(5):
+                outcome = client.report_outcome(number)
+                results.append((number, outcome))
+
+    return results
+
+
+def main():
+    address = ('127.0.0.1', 1234)
+    server_thread = Thread(
+        target=run_server, args=(address,), daemon=True)
+    server_thread.start()
+
+    results = run_client(address)
+    for number, outcome in results:
+        print(f'클라이언트: {number}는 {outcome}')
+
+main()
